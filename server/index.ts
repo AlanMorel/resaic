0408a854a14a18a767eedd5fb7deb53e5a0a2552
Alan Morel <alan@alanmorel.com>
2020-09-10 22:47:54 -0400

    Adding body-parser, cookie-parser, cors, and helmet dependencies

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,25 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
 
 const app = express();
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(
+    helmet({
+        contentSecurityPolicy: false
+    })
+);
+
 const PORT = 8000;
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
-    console.log(`[server]: Server is running at http://localhost:${PORT}`);
-});
+app.listen(PORT);
+
+console.log(`[server]: Server is running at http://localhost:${PORT}`);
