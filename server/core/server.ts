49cc3ec4a04c53bde96d234455bd86988669ef71
Alan Morel <alan@alanmorel.com>
2020-09-14 14:28:31 -0400

    Adding express session

--- server/core/server.ts
+++ server/core/server.ts
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
+import session from "express-session";
 import staticAssets from "./static";
 import passport from "./passport";
 import handlebars from "./handlebars";
@@ -18,6 +19,13 @@ app.use(cors());
 app.use(
     helmet({
         contentSecurityPolicy: false
+    })
+);
+app.use(
+    session({
+        secret: config.sessionSecret,
+        resave: false,
+        saveUninitialized: false
     })
 );
 
