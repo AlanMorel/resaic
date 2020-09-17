90a17217b804167b4632ae17a861af396933797d
Alan Morel <alan@alanmorel.com>
2020-09-17 14:21:53 -0400

    Add compression npm package, move eslint declarations into respective folders

--- server/core/server.ts
+++ server/core/server.ts
@@ -3,6 +3,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import cors from "cors";
+import compression from "compression";
 import helmet from "helmet";
 import session from "express-session";
 import staticAssets from "./static";
@@ -15,6 +16,7 @@ const app = express();
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(cors());
+app.use(compression());
 app.use(
     helmet({
         contentSecurityPolicy: false
