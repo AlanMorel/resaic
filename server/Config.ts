8734bd31aea0ed7cd21418f68947b3c6d3e7a81a
Alan Morel <alan@alanmorel.com>
2021-08-23 13:34:36 -0400

    Adding renames

---
+++ server/Config.ts
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+import fs from "fs";
+
+dotenv.config();
+
+const name = "Resaic";
+const port = parseInt(process.env.PORT || "8083");
+const origin = process.env.ORIGIN || "http://localhost:" + port;
+const env = process.env.NODE_ENV || "development";
+const root = __dirname.split("dist")[0].split("server")[0];
+const version = fs.readFileSync(root + "/version.txt");
+const secret = process.env.SESSION_SECRET || "secret";
+
+export default {
+    name,
+    port,
+    origin,
+    env,
+    root,
+    version,
+    secret
+};
---
+++ server/database/Database.ts
@@ -0,0 +1,22 @@
+import { Sequelize } from "sequelize";
+import { ContactFactory } from "./models/Contact";
+import { UserFactory } from "./models/User";
+
+const Database = new Sequelize({
+    host: process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_TABLE,
+    port: parseInt(process.env.DATABASE_PORT ?? "3306"),
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    }
+});
+
+export const User = UserFactory(Database);
+export const Contact = ContactFactory(Database);
+
+export default Database;
